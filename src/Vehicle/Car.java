cedd739b01f6a269460c5222600fb944da7014ab
zeki <zekiursavas07@gmail.com>
2023-08-10 19:40:39 -0400

    Variables and gettter and setter were added to car class, Methood1 was added to plane class

--- src/Vehicle/Car.java
+++ src/Vehicle/Car.java
@@ -1,6 +1,34 @@
 package Vehicle;
 
 public class Car {
+    String make;
+    String model;
+    int year;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     public static void main(String[] args) {
         for (int i = 0; i <10 ; i++) {
             System.out.println("car goes");
--- src/Vehicle/Plane.java
+++ src/Vehicle/Plane.java
@@ -4,4 +4,8 @@ public class Plane {
     public static void main(String[] args) {
         System.out.println("plane flies");
     }
+
+    public void method1(){
+        System.out.println("Planes go fast");
+    }
 }
